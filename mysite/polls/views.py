4feab5caf3476fea7ae08daa5cea6aec1bd0df7a
wkokgit <wouterkok96@hotmail.com>
2020-04-15 08:31:21 +0200

    step 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -49,3 +50,9 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+def get_queryset(self):
+    """
+    Excludes any questions that aren't published yet.
+    """
+	return Question.objects.filter(pub_date__lte=timezone.now())

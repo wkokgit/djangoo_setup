ac5526ad09bc2b06bae84a6858bd0d98511daaf5
wkokgit <wouterkok96@hotmail.com>
2020-04-15 10:45:41 +0200

    step 7

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -30,7 +30,7 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def vote(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -55,4 +55,4 @@ def get_queryset(self):
     """
     Excludes any questions that aren't published yet.
     """
-	return Question.objects.filter(pub_date__lte=timezone.now())
+    return Question.objects.filter(pub_date__lte=timezone.now())

4225eec7e852e4f5291a377b3b15062417c0f388
wkokgit <wouterkok96@hotmail.com>
2020-04-15 09:48:21 +0200

    step 6

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -30,7 +30,7 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+	question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):

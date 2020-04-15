5c65be7edcc7e4129bfc71f165cde98d0b145dc5
wkokgit <wouterkok96@hotmail.com>
2020-04-15 07:55:35 +0200

    Step 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Question
+# ...
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
